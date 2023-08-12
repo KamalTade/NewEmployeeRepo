4f5c74975267f2473e36e28e381e07c3d75e0ae5
KamalTade <tadekamal2727@gmail.com>
2023-08-12 17:14:00 +0530

    first commit

---
+++ src/main/java/in/ashokit/GraphApp7Application.java
@@ -0,0 +1,13 @@
+package in.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphApp7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphApp7Application.class, args);
+	}
+
+}
